7183b6e7a3a065d444e50e415a8d2e0f62345801
Zhou Tianxing <zhoutianxing@taizhou0523.cn>
2020-06-11 10:43:00 +0800

    Initial commit
    Signed-off-by: Zhou Tianxing <zhoutianxing@taizhou0523.cn>

---
+++ src/main/java/cn/taizhou0523/vadmin/VadminApplication.java
@@ -0,0 +1,13 @@
+package cn.taizhou0523.vadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/taizhou0523/vadmin/VadminApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.taizhou0523.vadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
